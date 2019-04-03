6bb83de1199f49c5c4bd6bb3aafa774c884c7952
harikrishnankr <hari@qburst.com>
2019-04-03 18:37:53 +0530

    Implemented routing

---
+++ src/components/login/index.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import s from "./login.scss";
+
+class Login extends Component {
+    render() {
+        return <div className={s.intro}>Login</div>;
+    }
+}
+export default Login;
