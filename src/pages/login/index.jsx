922c496e2e7741e5660d598ed65ed0e1489c114d
hrikrishnankr <hari@qburst.com>
2019-04-09 00:18:40 +0530

    added shared components

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+
+import Input from "../../shared/input";
+import Button from "../../shared/button";
 import "./login.scss";
 
 class Login extends Component {
@@ -9,8 +12,29 @@ class Login extends Component {
 	
     render() {
         return (
-        	<div>
-        		Login
+        	<div className="container">
+        		<div className="left-side">
+                    <div className="logo"></div>
+                    <div className="sign-in-form">
+                        <h2>Login</h2>
+                        <form>
+                            <div className="form-group">
+                                <Input/>
+                            </div>
+                            <div className="form-group">
+                                <Input/>
+                            </div>
+                            <div className="form-group button-group">
+                                <Button text="Sign In"/>
+                                <Button text="Sign Up"/>
+                            </div>
+                            <div className="form-group social-networks"></div>
+                        </form>
+                    </div>
+                </div>
+                <div className="right-side">
+
+                </div>
         	</div>
         );
     }
